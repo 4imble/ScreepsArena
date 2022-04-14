f3782fe70e2611278589decce03979b2835c15b3
4imble <gitkraken@4imble.net>
2022-04-14 21:40:25 +0100

    fixed build errors

--- src/arena_tutorial/main.ts
+++
@@ -1,9 +0,0 @@
-import { getObjectsByPrototype } from 'game/utils';
-import { Creep } from 'game/prototypes';
-import { Flag } from 'arena/prototypes';
-
-export function loop() {
-    var creeps = getObjectsByPrototype(Creep);
-    var flags = getObjectsByPrototype(Flag);
-    creeps[0].moveTo(flags[0]);
-}
